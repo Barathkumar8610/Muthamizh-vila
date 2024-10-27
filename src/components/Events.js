5ae37a272c4b08fa7a0bf170d3ffb7a6e3760e2f
Barathkumar8610 <barathkumar8610@gmail.com>
2024-10-27 14:12:59 +0530

    bgimage_changed

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,12 +2,72 @@ import React from 'react';
 import '../styles/Events.css';
 
 const Event = () => {
+
+
   return (
-    <section id="home">
-      <h1>Welcome to Muthamizh Vila</h1>
-      <p>Experience the cultural extravaganza of Tamil heritage.</p>
-      <button>Learn More</button>
-    </section>
+     <div id="Event">
+        <div class="arrow">
+           <img src="arrow.png" alt="Arrow"></img>
+        </div>
+        <div class="content">
+        <div class="eventTitle">
+            <h1>இயற்த்தமிழ்</h1>
+        </div>
+        <div class="eventList1">
+           <div class="details">
+              <div class="eventHeading">
+                பேச்சுப்போட்டி : 
+              </div>
+              <br />
+              <div class="eventContent">
+                செவி கேட்ட செய்திகளைச் செவி கேட்கும் படி உரக்கப் பேச பேச்சுப்போட்டி.
+              </div>
+           </div>
+
+           <div class="details">
+           <div class="eventHeading">
+                கவிதை :  
+              </div>
+              <br />
+              <div class="eventContent">
+              நெஞ்சைக் கவர்ந்த வார்த்தைகளின் பிணைப்பைக் காகிதத்தில் கரை சேர்க்கக் கவிதைப் போட்டி.
+              </div>
+           </div>
+
+           <div class="details">
+           <div class="eventHeading">
+              தமிழ் வினாடி வினா:  
+              </div>
+              <br />
+              <div class="eventContent">
+              தமிழ் குறித்து உங்கள் அறிதல்களைத் தமிழில் விடை எழுதத் தமிழ் வினாடி வினா.
+              </div>
+           </div>
+
+
+           <div class="details">
+           <div class="eventHeading">
+              தமிழ் வினாடி வினா:  
+              </div>
+              <br />
+              <div class="eventContent">
+              தமிழ் குறித்து உங்கள் அறிதல்களைத் தமிழில் விடை எழுதத் தமிழ் வினாடி வினா.
+              </div>
+           </div>
+
+
+           <div class="details">
+           <div class="eventHeading">
+              தமிழ் வினாடி வினா:  
+              </div>
+              <br />
+              <div class="eventContent">
+              தமிழ் குறித்து உங்கள் அறிதல்களைத் தமிழில் விடை எழுதத் தமிழ் வினாடி வினா.
+              </div>
+           </div>
+        </div>
+        </div>
+     </div>
   );
 };
 
